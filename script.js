1d93c07ceb03fa23b4c05a860612178afb53f10d
Ihor Mirlin <imirlin@gmail.com>
2021-12-20 10:41:19 +0000

    (2021-12-20) Updated: changes due to lab's tasks

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal * years * rate /100;
+
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document
+        .getElementById("result")
+        .innerHTML = "<hr/>\
+        <b>Interest:</b>\
+        <span>If you deposit <mark>" + principal + "</mark>,</span>\
+        <span>at an interest rate of <mark>" + rate + "%</mark></span>\
+        <span>You will receive an amount of <mark>" +amount + "</mark>,</span>\
+        <span>in the year <mark> " + year + " </mark></span>"
     
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=`${rateval} %`;
 }
         
